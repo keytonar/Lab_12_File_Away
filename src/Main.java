188b53e6d84fd7e4a4b738874485d86dec292cb1
keytonar <austinkeyton@gmail.com>
2023-04-10 13:55:03 -0400

    Finished jfilechoose project now uses jfilechooser to have the user select a file on their machine and displays the File name, number of lines, number of words, and Number of characters aswell as the file length.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
+import javax.swing.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            String fileName = file.getName();
+            int lineCount = 0;
+            int wordCount = 0;
+            int charCount = 0;
+            int fileLength = 0;
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    lineCount++;
+                    String[] words = line.split("\\s+");
+                    wordCount += words.length;
+                    charCount += line.length();
+                    fileLength += line.length() + 1; // +1 for line separator
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("File Name: " + fileName);
+            System.out.println("Number of Lines: " + lineCount);
+            System.out.println("Number of Words: " + wordCount);
+            System.out.println("Number of Characters: " + charCount);
+            System.out.println("File Length: " + fileLength);
+        }
     }
 }
